0ed996b8c4f5518dd4cfc344f7dd9bb13fd43f13
Sergii Ponomarov <sergii.ponomarov@gmail.com>
2013-02-05 23:30:08 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skeleton::Application.config.secret_token = 'f819e465610617da4da723628e0db38cba5f3825fcfaee859397044d39844622845b70945aeca616d502c47c7359f88da5f0d426a0992c8b1a9e69e218e6ab90'
